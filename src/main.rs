a846331593717771019b687456a77c20c0c2ee18
Andrew Prindle <prindle.andrew@gmail.com>
2022-09-10 14:31:59 -0500

    Changed main to draw an annulus (for perf)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-use arc_test::{setup, Arc, CENTER, RADIUS};
-
+use arc_test::{CENTER, RADIUS};
 const RADS: f64 = std::f64::consts::PI / 4.0;
-const START: f64 = RADS * 0.1;
-const END: f64 = RADS * 7.75;
 
-// fn main() -> Result<(), image::ImageError> {
-fn main() {
+fn main() -> Result<(), image::ImageError> {
     use criterion::black_box;
-    let mut image = setup(RADIUS);
-    println!("creating arc");
-    let mut arc = Arc::new(START, END, RADIUS, CENTER.into());
-    println!("drawing");
-    arc.draw(&mut image, image::Rgba([255, 0, 0, 255]));
+    let mut image = arc_test::setup(RADIUS);
+
+    // DRAW PARTIAL ARC
+    // const START: f64 = RADS * 0.1;
+    // const END: f64 = RADS * 7.75;
+    // let mut arc = arc_test::Arc::new(START, END, RADIUS, CENTER.into());
+    // arc.draw(&mut image, image::Rgba([255, 0, 0, 255]));
+
+    // DRAW PARTIAL ANNULUS
+    let ri = crate::RADIUS - 10;
+    let ro = crate::RADIUS;
+    let start = RADS * 6.00;
+    let end = RADS * 7.0 - std::f64::EPSILON;
+    let mut an = arc_test::Annulus::new(start, end, ri, ro, crate::CENTER.into());
+    an.draw(&mut image, image::Rgba([255, 0, 0, 255]));
+
     let _b = black_box(image);
-    // image.save("images/main.png")
+    if _b.height() > 0 {
+        Ok(())
+    } else {
+        Err(image::ImageError::Unsupported(
+            image::error::UnsupportedError::from_format_and_kind(
+                image::error::ImageFormatHint::Name("Invalid dimensions".into()),
+                image::error::UnsupportedErrorKind::GenericFeature("Invalid image size".into()),
+            ),
+        ))
+    }
 }
