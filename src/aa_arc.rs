e6efb892ec490a120eddee39647b0c52f0263455
Andrew Prindle <prindle.andrew@gmail.com>
2022-09-13 11:48:06 -0500

    Cleaned up println's in aa_arc

--- src/aa_arc.rs
+++ src/aa_arc.rs
@@ -1,10 +1,11 @@
 mod aa_oct;
 
-mod aa_quad {
+pub(crate) mod aa_quad {
     use crate::Pt;
+    use log::{debug, trace};
 
     #[derive(Clone, Debug)]
-    struct Iter {
+    pub struct AAArc {
         x: f64,
         y: f64,
         r: f64,
@@ -14,8 +15,8 @@ mod aa_quad {
         end: u8,  // end quadrant
         c: Pt<f64>,
     }
-    impl Iter {
-        fn start(r: i32, c: Pt<i32>) -> Self {
+    impl AAArc {
+        pub fn start(r: i32, c: Pt<i32>) -> Self {
             let r = r as f64;
             Self {
                 x: 0.0,
@@ -53,12 +54,12 @@ mod aa_quad {
             (a, b, o)
         }
 
-        fn step(&mut self) -> (Pt<u32>, Pt<u32>, f64) {
+        fn step(&mut self) -> Option<(Pt<u32>, Pt<u32>, f64)> {
             let (x, y) = (self.x, self.y);
             let rst;
             if self.x <= self.y {
                 let (a, b, o) = Self::calc_fract(self.y);
-                println!("Oct7 x={} ya={} yb={} o={:.2}", self.x, a, b, o);
+                debug!("Oct7 x={} ya={} yb={} o={:.2}", self.x, a, b, o);
                 rst = (
                     Pt::new(x, a).quad_to_iter(self.quad, self.c).u32(),
                     Pt::new(x, b).quad_to_iter(self.quad, self.c).u32(),
@@ -68,7 +69,7 @@ mod aa_quad {
                 self.y = self.calc_slow(self.x);
             } else {
                 let (a, b, o) = Self::calc_fract(self.x);
-                println!("Oct8 y={} xa={} xb={} o={:.2}", self.y, a, b, o);
+                debug!("Oct8 y={} xa={} xb={} o={:.2}", self.y, a, b, o);
                 rst = (
                     Pt::new(a, y).quad_to_iter(self.quad, self.c).u32(),
                     Pt::new(b, y).quad_to_iter(self.quad, self.c).u32(),
@@ -77,15 +78,15 @@ mod aa_quad {
                 self.y -= 1.0;
                 self.x = self.calc_slow(self.y);
             }
-            println!("    {:?}", rst);
-            rst
+            trace!("    {:?}", rst);
+            Some(rst)
         }
         fn end(&self) -> bool {
             self.quad == self.end && self.y <= 0.0
         }
     }
 
-    impl Iterator for Iter {
+    impl Iterator for AAArc {
         type Item = (Pt<u32>, Pt<u32>, f64);
         fn next(&mut self) -> Option<Self::Item> {
             if self.end() {
@@ -94,8 +95,7 @@ mod aa_quad {
             if self.next_quad() {
                 return self.next();
             }
-            let rst = self.step();
-            Some(rst)
+            self.step()
         }
     }
 
@@ -112,23 +112,11 @@ mod aa_quad {
         (d * 255.0).round().rem(256.0) as u8
     }
 
-    fn draw(image: &mut image::RgbaImage, iter: Iter, color: image::Rgba<u8>) {
-        // let mut switch = false;
+    pub fn draw(image: &mut image::RgbaImage, iter: AAArc, color: image::Rgba<u8>) {
         for (a, b, d) in iter {
             let o = opac(d);
             plot_aa(image, a, b, o, color);
-            // if (!switch && a.x + 1 > a.y) {
-            //     println!("about to switch at {:?}", a);
-            //     image.put_pixel(a.x(), a.y() - 1, image::Rgba([0, 0, 255, 255]));
-            //     image.put_pixel(b.x(), b.y() + 1, image::Rgba([0, 0, 255, 255]));
-            // }
-            // if (!switch && a.x > a.y) {
-            //     println!("switched at {:?}", a);
-            //     switch = true;
-            //     image.put_pixel(a.x() - 1, a.y(), image::Rgba([0, 255, 0, 255]));
-            //     image.put_pixel(b.x() + 1, b.y(), image::Rgba([0, 255, 0, 255]));
-            // }
-            println!("    plot: o={} a={:?} b={:?}", o, a, b);
+            trace!("    plot: o={} a={:?} b={:?}", o, a, b);
         }
     }
 
@@ -137,8 +125,9 @@ mod aa_quad {
         use super::*;
         #[test]
         fn aa_iter() -> Result<(), image::ImageError> {
+            crate::logger(log::LevelFilter::Debug);
             let mut image = crate::guidelines();
-            let iter = Iter::start(crate::RADIUS, crate::CENTER.into());
+            let iter = AAArc::start(crate::RADIUS, crate::CENTER.into());
             let color = image::Rgba([255, 0, 0, 255]);
             draw(&mut image, iter, color);
             image.save("images/aa_iter.png")
