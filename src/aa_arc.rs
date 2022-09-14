75f8e04d2b568c864f7eda73b43d0fcd3875bc33
Andrew Prindle <prindle.andrew@gmail.com>
2022-09-13 20:44:52 -0500

    Added a non-iterator draw method to AAArc
    Also added a draw() method to AAPt

--- src/aa_arc.rs
+++ src/aa_arc.rs
@@ -2,7 +2,7 @@ mod aa_oct;
 
 pub(crate) mod aa_quad {
     use crate::Pt;
-    use log::{debug, trace};
+    use log::debug;
 
     pub struct AAPt<T> {
         a: Pt<T>,
@@ -12,6 +12,7 @@ pub(crate) mod aa_quad {
     }
     impl<T> AAPt<T> {
         fn new(a: Pt<T>, b: Pt<T>, da: f64) -> Self {
+            // debug!("da={} -> db={}", da, 1.0 - da);
             Self {
                 a,
                 b,
@@ -19,16 +20,25 @@ pub(crate) mod aa_quad {
                 db: 1.0 - da,
             }
         }
-        fn reduce_opac_a(self, i: f64) -> Self {
+        fn reduce_opac_b(self, i: f64) -> Self {
             Self {
                 a: self.a,
                 b: self.b,
-                da: self.da * i,
-                db: self.db,
+                da: self.da,
+                db: self.db * i,
             }
         }
     }
 
+    impl AAPt<u32> {
+        fn draw(&self, image: &mut image::RgbaImage, color: image::Rgba<u8>) {
+            use image::Pixel;
+            let c1 = alpha(opac(self.da), color);
+            let c2 = alpha(opac(self.db), color);
+            image.get_pixel_mut(self.a.x(), self.a.y()).blend(&c2);
+            image.get_pixel_mut(self.b.x(), self.b.y()).blend(&c1);
+        }
+    }
     enum End {
         X(f64),
         Y(f64),
@@ -134,19 +144,24 @@ pub(crate) mod aa_quad {
                     self.inc_x = false;
                     self.y = self.y.ceil();
                     debug!("switching");
-                    self.step_y().reduce_opac_a(0.0)
+                    // self.step_y().reduce_opac_a(0.0)
+                    self.step_y()
                 } else {
                     self.step_y()
                 }
             }
         }
 
+        fn reset(&mut self) {
+            self.x = 0.0;
+            self.y = self.r;
+            self.inc_x = true;
+            self.quad = self.quad % 4 + 1;
+        }
+
         fn next_quad(&mut self) -> bool {
             if self.y < 0.0 {
-                self.x = 0.0;
-                self.y = self.r;
-                self.inc_x = true;
-                self.quad = self.quad % 4 + 1;
+                self.reset();
                 true
             } else {
                 false
@@ -155,6 +170,26 @@ pub(crate) mod aa_quad {
 
         fn end(&self) -> bool {
             self.quad == self.end_quad && self.y <= 0.0
+        }
+
+        pub fn draw(mut self, image: &mut image::RgbaImage, color: image::Rgba<u8>) {
+            let ffd = (self.r / std::f64::consts::SQRT_2).round() as u32; //forty-five degree point
+            loop {
+                for _ in 0..ffd {
+                    self.step_x().draw(image, color);
+                }
+                self.y = self.y.ceil();
+                self.step_y().reduce_opac_b(0.5).draw(image, color);
+
+                for _ in 1..ffd {
+                    self.step_y().draw(image, color);
+                }
+
+                if self.end() {
+                    break;
+                }
+                self.reset();
+            }
         }
     }
 
@@ -172,24 +207,18 @@ pub(crate) mod aa_quad {
         }
     }
 
-    fn plot_aa(image: &mut image::RgbaImage, a: Pt<u32>, b: Pt<u32>, o: u8, c: image::Rgba<u8>) {
-        use image::Pixel;
-        let c1 = image::Rgba([c[0], c[1], c[2], 255 - o]);
-        let c2 = image::Rgba([c[0], c[1], c[2], o]);
-        image.get_pixel_mut(a.x(), a.y()).blend(&c1);
-        image.get_pixel_mut(b.x(), b.y()).blend(&c2);
-    }
-
     fn opac(d: f64) -> u8 {
         use std::ops::Rem;
         (d * 255.0).round().rem(256.0) as u8
     }
 
+    fn alpha(a: u8, c: image::Rgba<u8>) -> image::Rgba<u8> {
+        image::Rgba([c[0], c[1], c[2], a])
+    }
+
     pub fn draw(image: &mut image::RgbaImage, iter: AAArc, color: image::Rgba<u8>) {
-        for AAPt { a, b, da, db } in iter {
-            let o = opac(da);
-            plot_aa(image, a, b, o, color);
-            trace!("    plot: o={} a={:?} b={:?}", o, a, b);
+        for pt in iter {
+            pt.draw(image, color);
         }
     }
 
@@ -204,6 +233,16 @@ pub(crate) mod aa_quad {
             let color = image::Rgba([255, 0, 0, 255]);
             draw(&mut image, iter, color);
             image.save("images/aa_iter.png")
+        }
+
+        #[test]
+        fn aa_draw() -> Result<(), image::ImageError> {
+            crate::logger(log::LevelFilter::Debug);
+            let mut image = crate::guidelines();
+            let color = image::Rgba([255, 0, 0, 255]);
+            let iter = AAArc::start(crate::RADIUS, crate::CENTER.into());
+            iter.draw(&mut image, color);
+            image.save("images/aa_draw.png")
         }
     }
 }
